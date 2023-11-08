b2f722a93e60b08c79dd29aae87dd7b048beb848
dejwid <dawid.paszko@gmail.com>
2023-11-08 19:04:42 +0100

    MY_AWS...

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -10,8 +10,8 @@ export async function POST(req) {
     const s3Client = new S3Client({
       region: 'us-east-1',
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
+        accessKeyId: process.env.MY_AWS_ACCESS_KEY,
+        secretAccessKey: process.env.MY_AWS_SECRET_KEY,
       },
     });
 
