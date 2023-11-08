6fb15ca23ef1543bf14976ea488cd9af8dd46bf3
dejwid <dawid.paszko@gmail.com>
2023-11-08 19:06:30 +0100

    key 1

--- src/components/layout/MenuItemPriceProps.js
+++ src/components/layout/MenuItemPriceProps.js
@@ -44,7 +44,7 @@ export default function MenuItemPriceProps({name,addLabel,props,setProps}) {
       </button>
       <div className={isOpen ? 'block' : 'hidden'}>
         {props?.length > 0 && props.map((size,index) => (
-          <div className="flex items-end gap-2">
+          <div key={index} className="flex items-end gap-2">
             <div>
               <label>Name</label>
               <input type="text"
